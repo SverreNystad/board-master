412b1cbb2a6e5d0371ca4b9e33771b6fb44af9d7
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-12-01 00:24:29 +0100

    Feat: Add TicTacToe Statehandler with stubs all needed methods from interface

---
+++ app/src/main/java/board/master/game/TicTacToe.java
@@ -0,0 +1,68 @@
+package board.master.game;
+
+import java.util.List;
+
+/**
+ * TicTacToe class implementing the StateHandler interface.
+ * This class manages the state of a Tic Tac Toe game, including the game board,
+ * player turns, and checking for game over conditions.
+ */
+public class TicTacToe implements StateHandler {
+
+    // Game board representation, player turn, and other necessary state variables
+    // TODO: Define the game board and other state variables
+
+    /**
+     * Constructs a new TicTacToe game with an empty board and sets the starting player.
+     */
+    public TicTacToe() {
+        // TODO: Initialize the game board and set the starting player
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int toMove() {
+        // TODO: Implement method to return the current player's turn
+        return 0; // Placeholder return
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Action> getActions() {
+        // TODO: Implement method to return a list of legal actions (moves) for the current state
+        return null; // Placeholder return
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void result(Action action) {
+        // TODO: Implement method to apply an action and update the game state
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isTerminal() {
+        // TODO: Implement method to check if the current state is a terminal state (game over)
+        return false; // Placeholder return
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int Utility() {
+        // TODO: Implement method to calculate the utility of the current state
+        return 0; // Placeholder return
+    }
+
+    // Additional helper methods specific to Tic Tac Toe, such as checking for a win, can be added here
+    // TODO: Implement helper methods for game-specific logic
+}
