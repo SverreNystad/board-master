daa2e75a27ecbb126625db4a926d1dbf61c62296
Sverre Nystad <sverrenystad@gmail.com>
2023-12-20 16:49:36 +0100

    Feat(#4): Create Agent interface

---
+++ app/src/main/java/board/master/agents/Agent.java
@@ -0,0 +1,23 @@
+package board.master.agents;
+
+import board.master.game.Action;
+import board.master.game.StateHandler;
+
+/**
+ * The Agent interface represents an agent that can play a game.
+ * It shall choose the best move to make in a given game state.
+ * 
+ * The Agent follows the strategy pattern:
+ * https://en.wikipedia.org/wiki/Strategy_pattern
+ */
+public interface Agent {
+
+  /**
+   * Calculates the best move to make in a given game state.
+   * 
+   * @param state The current state of the game.
+   * @return The best action to take in the current state.
+   */
+  public Action getAction(StateHandler state);
+
+}
