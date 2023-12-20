af17cd4b0640c0d919ac869798cb0c35741d3e68
Sverre Nystad <sverrenystad@gmail.com>
2023-12-20 16:50:34 +0100

    Feat(#4): Make minimax implement agent interface and rename method name

--- app/src/main/java/board/master/agents/Minimax.java
+++ app/src/main/java/board/master/agents/Minimax.java
@@ -1,4 +1,4 @@
-package board.master.algorithm;
+package board.master.agents;
 
 import board.master.game.StateHandler;
 import board.master.game.Action;
@@ -6,34 +6,36 @@ import board.master.game.Action;
 /**
  * Class implementing the Minimax algorithm.
  * 
- * Minimax is a recursive algorithm used for choosing optimal moves in 
- * game theory settings, particularly in turn-based games. This class provides 
- * methods to calculate the best possible move considering future potential moves 
- * of an opponent. It is designed to be used with games that can be represented 
+ * Minimax is a recursive algorithm used for choosing optimal moves in
+ * game theory settings, particularly in turn-based games. This class provides
+ * methods to calculate the best possible move considering future potential
+ * moves
+ * of an opponent. It is designed to be used with games that can be represented
  * through a StateHandler.
  */
-public class Minimax {
+public class Minimax implements Agent {
 
     /**
      * Calculates the best move to make in a given game state.
      * 
-     * This method applies the minimax algorithm to determine the optimal move. 
-     * It considers all possible moves, evaluates their outcomes, and chooses the 
+     * This method applies the minimax algorithm to determine the optimal move.
+     * It considers all possible moves, evaluates their outcomes, and chooses the
      * move that maximizes the player's chances of winning.
      *
      * @param state The current state of the game.
      * @return The best action to take in the current state.
      */
-    public static Action calculateBestMove(StateHandler state) {
+    @Override
+    public Action getAction(StateHandler state) {
         // TODO: Implement the minimax algorithm to choose and return the best action.
-        throw new UnsupportedOperationException("Method not implemented yet.");
+        throw new UnsupportedOperationException("Unimplemented method 'getAction'");
     }
 
     /**
      * Evaluates the given state of the game.
      * 
      * The evaluation function assigns a numerical value to a game state,
-     * indicating how favorable it is to the player using the minimax algorithm. 
+     * indicating how favorable it is to the player using the minimax algorithm.
      * A more positive value indicates a more favorable state.
      *
      * @param state The game state to evaluate.
@@ -44,7 +46,8 @@ public class Minimax {
         throw new UnsupportedOperationException("Method not implemented yet.");
     }
 
-    // Additional methods for optimizations (like alpha-beta pruning) and utility functions can be added here
+    // Additional methods for optimizations (like alpha-beta pruning) and utility
+    // functions can be added here
     // TODO: Consider implementing alpha-beta pruning for optimization
 
 }
