3b719bbd4d0706598f6c6277436e67f112b8129d
Sverre Nystad <sverrenystad@gmail.com>
2023-12-21 23:12:20 +0100

    Feat: Add two agents to controller

--- app/src/main/java/board/master/controller/GameController.java
+++ app/src/main/java/board/master/controller/GameController.java
@@ -1,31 +1,45 @@
 package board.master.controller;
 
+import board.master.view.View;
+
 import board.master.model.Action;
 import board.master.model.GameFactory;
+import board.master.view.ViewFactory;
 import board.master.model.StateHandler;
 import board.master.model.agents.Agent;
-import board.master.model.agents.RandomStrategy;
+import board.master.model.agents.AgentFactory;
 
 public class GameController {
 
-  private StateHandler game;
-  private Agent agent;
+    private StateHandler model;
+    private Agent firstAgent;
+    private Agent secondAgent;
+    private View view;
 
-  public GameController(String gameName, String agentStrategy) {
-    this.game = GameFactory.getGame(gameName);
-    this.agent = new RandomStrategy();
-  }
-
-  public void startGame() {
-    Action action = null;
-    while (true) {
-      action = agent.getAction(game);
-      game.result(action);
-      System.out.println(game);
-      if (game.isTerminal()) {
-        break;
-      }
+    public GameController(String gameName, String firstAgentStrategy, String secondAgentStrategy) {
+        this.model = GameFactory.getGame(gameName);
+        this.view = ViewFactory.getView(gameName);
+        this.firstAgent = AgentFactory.getAgent(firstAgentStrategy);
+        this.secondAgent = AgentFactory.getAgent(secondAgentStrategy);
     }
-  }
 
+    public void startGame() {
+        while (true) {
+            // Choose action
+            Action action = null;
+            if (model.toMove() == 1) {
+                action = firstAgent.getAction(model);
+            } else {
+                action = secondAgent.getAction(model);
+            }
+
+            // Update model and view
+            model.result(action);
+            view.updateBoard(model.getState());
+
+            if (model.isTerminal()) {
+                break;
+            }
+        }
+    }
 }
