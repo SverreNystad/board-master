d6cee8cf25ec266e46563bd1a2753cc3062ce9c6
Sverre Nystad <sverrenystad@gmail.com>
2023-12-23 00:09:03 +0100

    Feat: Add board impl and getBoard

--- app/src/main/java/board/master/model/game/tic_tac_toe/TicTacToe.java
+++ app/src/main/java/board/master/model/game/tic_tac_toe/TicTacToe.java
@@ -14,12 +14,13 @@ public class TicTacToe implements StateHandler {
 
     // Game board representation, player turn, and other necessary state variables
     // TODO: Define the game board and other state variables
-
+    private int[][] board;
     /**
      * Constructs a new TicTacToe game with an empty board and sets the starting
      * player.
      */
     public TicTacToe() {
+        this.board = new int[3][3];
         // TODO: Initialize the game board and set the starting player
     }
 
@@ -69,11 +70,27 @@ public class TicTacToe implements StateHandler {
         return 0; // Placeholder return
     }
 
+    private int getPosition(int x, int y) {
+        return this.board[x][y];
+    }
+
+    private void setPosition(int x, int y, int value) {
+        this.board[x][y] = value;
+    }
 
     public String getBoard() {
-        return "";
+        String boardRep = "";
+        for (int i = 0; i < this.board.length; i++) {
+            String row = "";
+            for (int j = 0; j < this.board[i].length; j++) {
+                row += this.board[i][j];
+            }
+            boardRep += row + "\n";
+        }
+        return boardRep;
     }
     // Additional helper methods specific to Tic Tac Toe, such as checking for a
     // win, can be added here
     // TODO: Implement helper methods for game-specific logic
+
 }
