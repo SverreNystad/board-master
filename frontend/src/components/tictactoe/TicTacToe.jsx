76008ac72cd7816c0876ce7390c890b520b58f06
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-12-28 22:05:07 +0100

    Feat: Create start game function in frontend

--- frontend/src/components/tictactoe/TicTacToe.jsx
+++ frontend/src/components/tictactoe/TicTacToe.jsx
@@ -1,15 +1,45 @@
-import React, { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../../routes/routeDefinitions.jsx';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-
+const baseUrl = 'http://localhost:8080/api';
+const startGameUrl = `${baseUrl}/start`;
+const makeMoveUrl = `${baseUrl}/move`;
+const botMoveUrl = `${baseUrl}/bot`;
 function TicTacToe() {
+  const [gameData, setGameData] = useState(null);
+
+  const startGame = async () => {
+    try {
+      console.log("Starting Game");
+      let requestData = {
+        playerColor: "white", // or "black"
+        botType: "minimax", // or "random"
+        gameType: "tic-tac-toe" // or "chess", based on your game logic
+      };
+      // Replace with your backend URL
+      const response = await axios.post(startGameUrl, requestData);
+
+      // Update state with response data
+      setGameData(response.data);
+      console.log("Game Started:", response.data);
+    } catch (error) {
+      console.error("Error starting game:", error);
+    }
+  }
+
+
 
   return (
     <div className="TicTacToe">
-        <h1>BoardMaster TicTacToe</h1>
-        
-      
+      <h1>BoardMaster TicTacToe</h1>
+      <button onClick={startGame}>Start Game</button>
+      {gameData && (
+        <div>
+          <p>Game ID: {gameData.gameId}</p>
+          <p>Status: {gameData.status}</p>
+          {/* Render more game data as needed */}
+        </div>
+      )}
     </div>
   );
 }
