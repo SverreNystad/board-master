615ad78e06ec813cce8276eea2636cdb0f8adac4
Jon Bergland <jobergl@stud.ntnu.no>
2023-12-29 17:37:43 +0100

    Refactor: Remove value variable

--- backend/src/main/java/board/master/model/Move.java
+++ backend/src/main/java/board/master/model/Move.java
@@ -6,18 +6,18 @@ public class Move extends Action {
     
     private final String x; // e.g., "e2"
     private final String y;   // e.g., "e4"
-    private final String value; // e.g., "1", "-1"
+    //private final String value; // e.g., "1", "-1"
     
     public Move(String x, String y) {
         this.x = x;
         this.y = y;
-        this.value = null;
+        //this.value = null;
     }
 
-    public Move (int x, int y, String value) {
+    public Move (int x, int y) {
         this.x = Integer.toString(x);
         this.y = Integer.toString(y);
-        this.value = value;
+        //this.value = value;
     }
 
     
@@ -30,7 +30,9 @@ public class Move extends Action {
         return y;
     }
 
+    /*
     public String getValue() {
         return value;
     }
+    */
 }
