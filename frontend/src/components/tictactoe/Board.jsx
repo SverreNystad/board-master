7029c4b825eb9f164b41cdb7d060dfa83b6bd4f5
Jon Bergland <jobergl@stud.ntnu.no>
2023-12-29 20:44:40 +0100

    Refactor: Move css to Board.css

--- frontend/src/components/tictactoe/Board.jsx
+++ frontend/src/components/tictactoe/Board.jsx
@@ -30,30 +30,6 @@ function Board({ grid, onClickCallback, shallLoad }) {
   );
 }
 
-const boardContainerStyle = {
-  position: 'relative'
-};
-
-const loadingOverlayStyle = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: 'rgba(255, 255, 255, 0.7)', // Semi-transparent overlay
-  zIndex: 1
-};
-
-const loadingSymbolStyle = {
-  padding: '10px',
-  backgroundColor: '#f0f0f0',
-  border: '1px solid #ccc',
-  borderRadius: '5px'
-};
-
 const cellStyle = {
   width: '50px',
   height: '50px',
