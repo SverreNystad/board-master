2a7b6088ea66d1c7589d77d78f4b65cb644f5abc
Jon Bergland <jobergl@stud.ntnu.no>
2023-12-30 00:16:45 +0100

    Feat: Make getAgents

---
+++ backend/src/main/java/board/master/controller/AgentController.java
@@ -0,0 +1,21 @@
+package board.master.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import board.master.model.agents.AgentFactory;
+
+@RestController
+@RequestMapping("/api")
+public class AgentController {
+    
+    @GetMapping("/agents")
+    public ResponseEntity<List<String>> getAgents() {
+        List<String> agents = AgentFactory.getAgentTypesList();
+        return ResponseEntity.ok(agents);
+    }
+}
