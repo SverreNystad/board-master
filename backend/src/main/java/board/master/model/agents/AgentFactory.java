4442734e264791c0b2c3c75a2ea441348e597531
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-12-29 16:10:02 +0100

    Feat(#7): Create getAgentTypes method to be implemented

--- backend/src/main/java/board/master/model/agents/AgentFactory.java
+++ backend/src/main/java/board/master/model/agents/AgentFactory.java
@@ -1,5 +1,7 @@
 package board.master.model.agents;
 
+import java.util.List;
+
 /**
  * Factory class for creating agents instances.
  */
@@ -15,6 +17,18 @@ public class AgentFactory {
             default:
                 throw new IllegalArgumentException("Invalid agent type: " + agentStrategy);
         }
+    }
+
+    /**
+     * Get the list of valid agent types.
+     * 
+     * @return list of valid agent types
+     */
+    public static List<String> getAgentTypes() {
+        // TODO Create a way to get the list of valid agent types that will be updated when new agents are added
+        // Maybe use a hashmap to store the agent types and their descriptions
+        // And return the keys of the hashmap, make then this hashmap used in createAgent method to make them stay in sync
+        return null;
     }
 
 }
