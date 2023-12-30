d46ad5e79bc0a7acbffdc03a1c655a53cd93e3a7
Sverre Nystad <sverrenystad@gmail.com>
2023-12-30 02:26:26 +0100

    Test: Create GameServiceTest

---
+++ backend/src/test/java/board/master/service/GameServiceTest.java
@@ -0,0 +1,20 @@
+package board.master.service;
+
+import org.junit.jupiter.api.Test;
+
+public class GameServiceTest {
+    @Test
+    void testBotMove() {
+
+    }
+
+    @Test
+    void testPlayerMove() {
+
+    }
+
+    @Test
+    void testStartGame() {
+
+    }
+}
