589bc0d048c6f0a7a83f707cc5d5161b72671776
Sverre Nystad <sverrenystad@gmail.com>
2023-12-30 03:56:51 +0100

    Test: Create tests for AgentFactory createAgent

---
+++ backend/src/test/java/board/master/model/agents/AgentFactoryTest.java
@@ -0,0 +1,26 @@
+package board.master.model.agents;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AgentFactoryTest {
+    @Test
+    @DisplayName("Test of createAgent with legal agent type")
+    void testCreateAgent() {
+        String agentType = AgentFactory.getAgentTypesList().get(0);
+        Agent agent = AgentFactory.createAgent(agentType);
+        assert(agent != null);
+        assert(agent instanceof Agent);
+    }
+
+    @Test
+    @DisplayName("Test of createAgent with illegal agent type")
+    void testCreateAgentIllegal() {
+        String agentType = "illegal";
+        assertThrows(IllegalArgumentException.class, () -> {
+            AgentFactory.createAgent(agentType);
+        });
+    }
+}
