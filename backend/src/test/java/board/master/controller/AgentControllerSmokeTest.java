47ae891ad7c76aaa598777e136d97e4fd76925f5
Sverre Nystad <sverrenystad@gmail.com>
2023-12-30 14:22:58 +0100

    Test: Add AgentControllerSmokeTest for testing getAgents endpoint

---
+++ backend/src/test/java/board/master/controller/AgentControllerSmokeTest.java
@@ -0,0 +1,30 @@
+package board.master.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AgentControllerSmokeTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final String getAgentsEndpoint = "/api/agents";
+
+    @Test
+    @DisplayName("Test that getAgents works for legal request")
+    public void testGetAgents() throws Exception {
+        mockMvc.perform(get(getAgentsEndpoint)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
