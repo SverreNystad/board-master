3ac9c6087b68ce4780b06dd19314f3d6b48f61b7
Sverre Nystad <sverrenystad@gmail.com>
2024-01-03 05:06:42 +0100

    Feat(): Create MonteCarloTreeSearch agent and configure factory

---
+++ backend/src/main/java/board/master/model/agents/MonteCarloTreeSearch.java
@@ -0,0 +1,14 @@
+package board.master.model.agents;
+
+import board.master.model.Action;
+import board.master.model.StateHandler;
+
+public class MonteCarloTreeSearch implements Agent {
+
+    @Override
+    public Action getAction(StateHandler state) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAction'");
+    }
+    
+}
