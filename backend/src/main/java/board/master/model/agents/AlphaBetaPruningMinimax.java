8d21beae23d8b13a803923658d4a1a87d1d57ce0
Sverre Nystad <sverrenystad@gmail.com>
2024-01-03 04:35:47 +0100

    Docs: Add Javadocs to alpha beta pruning

--- backend/src/main/java/board/master/model/agents/AlphaBetaPruningMinimax.java
+++ backend/src/main/java/board/master/model/agents/AlphaBetaPruningMinimax.java
@@ -3,11 +3,23 @@ package board.master.model.agents;
 import board.master.model.Action;
 import board.master.model.StateHandler;
 
+/**
+ * Implementation of the Alpha-Beta Pruning version of the Minimax algorithm.
+ * This class is designed to function as an agent in a board game scenario,
+ * where it decides the best move to make given the current state of the game.
+ */
 public class AlphaBetaPruningMinimax implements Agent {
 
     private int agentPlayerId;
-
-
+    
+    /**
+     * Returns the best action to take in the current state.
+     * It evaluates possible actions and chooses the one with the highest value
+     * as determined by the alpha-beta pruning minimax algorithm.
+     *
+     * @param state The current state of the game.
+     * @return The best action to take for the current player.
+     */
     @Override
     public Action getAction(StateHandler state) {
         this.agentPlayerId = state.toMove();
@@ -26,7 +38,18 @@ public class AlphaBetaPruningMinimax implements Agent {
         return currentBestAction;
     }
 
-    private float evaluateState(StateHandler state, Boolean isMaximizingPlayer) {
+    /**
+     * Evaluates the given state of the game using the minimax algorithm with alpha-beta pruning.
+     * This method is recursive and alternates between maximizing and minimizing player turns,
+     * pruning branches that are not beneficial for the maximizing player.
+     *
+     * @param state The game state to evaluate.
+     * @param alpha The alpha value for pruning (best already explored option along the path for the maximizer).
+     * @param beta The beta value for pruning (best already explored option along the path for the minimizer).
+     * @param isMaximizingPlayer True if the current move is by the maximizing player, false otherwise.
+     * @return The evaluated score of the game state.
+     */
+    private float evaluateState(StateHandler state, float lower_bound, float upper_bound, Boolean isMaximizingPlayer) {
 
         if (state.isTerminal()) {
             return state.utility(agentPlayerId);
