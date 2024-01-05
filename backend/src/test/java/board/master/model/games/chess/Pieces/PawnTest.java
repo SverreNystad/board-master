ae5eaea425fbd0a59a94dd87138a3d5c44373ec9
Jon Bergland <jobergl@stud.ntnu.no>
2024-01-05 16:16:01 +0100

    Feat: Add test for valid moves

--- backend/src/test/java/board/master/model/games/chess/Pieces/PawnTest.java
+++ backend/src/test/java/board/master/model/games/chess/Pieces/PawnTest.java
@@ -33,7 +33,7 @@ public class PawnTest {
     @Test
     @DisplayName("Test Pawn Symbol")
     void testGetSymbol() {
-        String symbol = "P";
+        String symbol = "PW";
         assertEquals(symbol, pawn.getSymbol());
     }
 
@@ -52,9 +52,44 @@ public class PawnTest {
     void testGetValidMoves1() {
         Pawn wronglyPlacedPawn = new Pawn(Color.WHITE, 0, 1);
         int validMoves = 0;
-        List<Action> list = wronglyPlacedPawn.getValidMoves(board);
         assertEquals(validMoves, wronglyPlacedPawn.getValidMoves(board).size());
     }
+
+    @Test
+    @DisplayName("Test Pawn isValidMove when in front of black piece")
+    void testGetValidMoves2() {
+        int x = 5;
+        int y = 1;
+        Pawn oppositePawn = new Pawn(Color.BLACK, x, y);
+        board.setPosition(x, y, oppositePawn.getSymbol());
+        int validMoves = 0;
+        assertEquals(validMoves, pawn.getValidMoves(board).size());
+    }
+
+    @Test
+    @DisplayName("Test Pawn isValidMove when in front of white piece")
+    void testGetValidMoves3() {
+        int x = 5;
+        int y = 1;
+        Pawn oppositePawn = new Pawn(Color.WHITE, x, y);
+        board.setPosition(x, y, oppositePawn.getSymbol());
+        int validMoves = 0;
+        List<Action> list = pawn.getValidMoves(board);
+        assertEquals(validMoves, pawn.getValidMoves(board).size());
+    }
+
+    @Test
+    @DisplayName("Test Pawn isValidMove when black piece is diagonal to it")
+    void testGetValidMoves4() {
+        int x = 5;
+        int y = 0;
+        Pawn oppositePawn = new Pawn(Color.BLACK, x, y);
+        board.setPosition(x, y, oppositePawn.getSymbol());
+        int validMoves = 2;
+        List<Action> list = pawn.getValidMoves(board);
+        assertEquals(validMoves, pawn.getValidMoves(board).size());
+    }
+
 
     @Test
     void testIsValidMove() {
