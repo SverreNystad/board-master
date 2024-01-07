e218b91eb945051e4d2ef44f69a4d70805b731af
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-01-07 18:17:03 +0100

    Refactor: Add startGame, makeMove, and botMove functions to gameService.js

--- frontend/src/services/gameService.js
+++ frontend/src/services/gameService.js
@@ -22,4 +22,65 @@ const getAgents = async () => {
 
 
 
-export  { getAgents };
+const startGame = async (gameType, botType) => {
+    try {
+      // Mock request data
+      let requestData = {
+          gameType: gameType,
+          botType: botType
+      };
+      const response = await axios.post(apiRoutes.startGame, requestData);
+      return response.data;
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
+/**
+ * Accesses the backend API to make a move
+ * @param {object} gameData The game data containing the game ID
+ * @param {number} x The x coordinate of the move
+ * @param {number} y The y coordinate of the move
+ * @returns {Promise} Resolves to the updated game data
+ * @throws {Error} If the move is invalid
+ * @throws {Error} If the game is already over
+ * @throws {Error} If the game is not found
+ */
+const makeMove = async (gameData, x, y) => {
+    console.log("Making Move" + x + y);
+    let requestBoard = {
+      gameId: gameData.gameId,
+      x: x,
+      y: y
+    } 
+
+    try {
+      const response = await axios.post(apiRoutes.makeMove, requestBoard);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+}
+
+/**
+ * Accesses the backend API to get the bot's move
+ * @param {object} gameData The game data containing the game ID
+ * @returns {Promise} Resolves to the updated game data
+ * @throws {Error} If the game is already over
+ * @throws {Error} If the game is not found
+ */
+const botMove = async (gameData) => {
+    let requestBody = {
+        gameId: gameData.gameId,
+    }
+
+    try {
+        const response = await axios.post(apiRoutes.botMove, requestBody);
+        return response.data;
+    } catch (error) {
+        throw error;
+    } 
+}
+
+export  { getAgents, startGame, makeMove, botMove };
