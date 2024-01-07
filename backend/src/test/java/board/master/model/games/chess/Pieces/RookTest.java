1f178bc9fd66880033d33bad18ff56d88f2a4008
Jon Bergland <jobergl@stud.ntnu.no>
2024-01-07 16:01:55 +0100

    Feat: Initialize tests for Rook

---
+++ backend/src/test/java/board/master/model/games/chess/Pieces/RookTest.java
@@ -0,0 +1,40 @@
+package board.master.model.games.chess.Pieces;
+
+import org.junit.jupiter.api.Test;
+
+public class RookTest {
+    @Test
+    void testGetColor() {
+
+    }
+
+    @Test
+    void testGetColumn() {
+
+    }
+
+    @Test
+    void testGetRow() {
+
+    }
+
+    @Test
+    void testGetSymbol() {
+
+    }
+
+    @Test
+    void testGetValidMoves() {
+
+    }
+
+    @Test
+    void testIsValidMove() {
+
+    }
+
+    @Test
+    void testMove() {
+
+    }
+}
