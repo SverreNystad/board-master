8ff3c01ae33e6bc2755adea6637623596f4ae9c9
Jon Bergland <jobergl@stud.ntnu.no>
2024-01-11 17:40:59 +0100

    Feat: Initialize King-class

---
+++ backend/src/main/java/board/master/model/games/chess/Pieces/King.java
@@ -0,0 +1,37 @@
+package board.master.model.games.chess.Pieces;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import board.master.model.Action;
+import board.master.model.games.Board;
+import board.master.model.games.chess.Color;
+
+public class King extends Piece {
+
+
+    public King(Color color, int row, int column) {
+        super(color, row, column);
+        super.symbol = "K";
+    }
+
+    public List<Action> getValidMoves(Board board) {
+        List<Action> actions = new ArrayList<Action>();
+
+        //test the row in front of the king
+        checkMove(board, this.row - 1, this.column - 1, actions);
+        checkMove(board, this.row - 1, this.column, actions);
+        checkMove(board, this.row - 1, this.column + 1, actions);
+
+        //test the row the king is on
+        checkMove(board, this.row, this.column - 1, actions);
+        checkMove(board, this.row, this.column + 1, actions);
+
+        //test the row behind the king
+        checkMove(board, this.row + 1, this.column - 1, actions);
+        checkMove(board, this.row + 1, this.column, actions);
+        checkMove(board, this.row + 1, this.column + 1, actions);
+
+        return actions;
+    }
+}
