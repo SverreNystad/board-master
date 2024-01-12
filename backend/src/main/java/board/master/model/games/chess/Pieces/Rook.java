92b07e29aa8cad00ac31b32429df30c84f075142
Jon Bergland <jobergl@stud.ntnu.no>
2024-01-12 17:34:34 +0100

    Refactor: Add do-while for check of moves

--- backend/src/main/java/board/master/model/games/chess/Pieces/Rook.java
+++ backend/src/main/java/board/master/model/games/chess/Pieces/Rook.java
@@ -31,28 +31,33 @@ public class Rook extends Piece {
         List<Action> actions = new ArrayList<Action>();
 
         //test for horizontal moves left
-        int left = this.column - 1;
-        while (checkMove(board, this.row, left, actions)) {
+        int left = this.column;
+        do {
             left--;
-        }
+            checkMove(board, this.row, left, actions);
+        } while (checkEmptySpace(board, this.row, left));
 
         //test for horizontal moves right
-        int right = this.column + 1;
-        while (checkMove(board, this.row, right, actions)) {
+        int right = this.column;
+        do {
             right++;
-        }
+            checkMove(board, this.row, right, actions);
+        } while (checkEmptySpace(board, this.row, right));
 
         //test for vertical moves up
-        int up = this.row - 1;
-        while (checkMove(board, up, this.column, actions)) {
+        int up = this.row;
+        do {
             up--;
-        }
+            checkMove(board, up, this.column, actions);
+        } while (checkEmptySpace(board, up, this.column));
 
         //test for vertical moves down
-        int down = this.row + 1;
-        while (checkMove(board, down, this.column, actions)) {
+        int down = this.row;
+        do {
             down++;
-        }
+            checkMove(board, down, this.column, actions);
+        } while (checkEmptySpace(board, down, this.column));
+
         return actions;
     }  
 }
