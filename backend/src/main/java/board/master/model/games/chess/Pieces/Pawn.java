7674ba41d4be9cdff78a910c31498be1de4a497d
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-01-12 18:38:29 +0100

    Refactor: pawn movement logic to use string coordinates

--- backend/src/main/java/board/master/model/games/chess/Pieces/Pawn.java
+++ backend/src/main/java/board/master/model/games/chess/Pieces/Pawn.java
@@ -50,13 +50,18 @@ public class Pawn extends Piece {
 
         // If the pawn is not at the end of the board, it can move forward one space
         if (checkEmptySpace(board, this.row + forwardDirection, this.column)) {
-            actions.add((Action) new Move(this.row + forwardDirection, this.column));
+            String currentPos = String.valueOf(this.row) + String.valueOf(this.column);
+            String newPos = String.valueOf(this.row + forwardDirection) + String.valueOf(this.column);
+            actions.add((Action) new Move(currentPos, newPos));
 
-             // If the pawn is at the start of the board, it can move one or two spaces
+            // If the pawn is at the start of the board, it can move one or two spaces
             if ((this.color == Color.BLACK && this.row == blackPawnRow) 
                 || (this.color == Color.WHITE && this.row == whitePawnRow)) {
                 if (checkEmptySpace(board, this.row + 2*forwardDirection, this.column)) {
-                    actions.add((Action) new Move(this.row + 2*forwardDirection, this.column));
+        
+                    currentPos = String.valueOf(this.row) + String.valueOf(this.column);
+                    newPos = String.valueOf(this.row + 2*forwardDirection) + String.valueOf(this.column);
+                    actions.add((Action) new Move(currentPos, newPos));
                 }
             }  
         }
