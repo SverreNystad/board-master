3816000e2393cdd5eba615aeed2c8432cb95ed5c
Jon Bergland <jobergl@stud.ntnu.no>
2024-01-16 21:41:57 +0100

    Feat: Add tests for isTerminal

--- backend/src/test/java/board/master/model/games/chess/ChessTest.java
+++ backend/src/test/java/board/master/model/games/chess/ChessTest.java
@@ -3,6 +3,9 @@ package board.master.model.games.chess;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,9 @@ import board.master.model.Action;
 import board.master.model.StateHandler;
 import board.master.model.games.Board;
 import board.master.model.games.Move;
+import board.master.model.games.chess.Pieces.King;
+import board.master.model.games.chess.Pieces.Piece;
+import board.master.model.games.chess.Pieces.Rook;
 
 public class ChessTest {
     private Chess chess;
@@ -58,6 +64,44 @@ public class ChessTest {
     @Test
     void testIsTerminal() {
 
+    }
+    @Nested
+    class testIsTerminal {
+        @Test
+        void testIsTerminalAtStart() {
+            assertEquals(false, chess.isTerminal(), "Game should not be terminal at start");
+        }
+
+        @Test
+        void testIsTerminalAfterOneMove() {
+            Action action = chess.getActions().get(0);
+            StateHandler transformed_chess = chess.result(action);
+            assertEquals(false, transformed_chess.isTerminal(), "Game should not be terminal after one move");
+        }
+
+        @Test
+        void testIsNotTerminalWithCheck() {
+            Board board = new Board(8,8);
+            Map<String, Piece> pieces = new HashMap<String, Piece>();
+
+            Piece whiteKing = new King(Color.WHITE, 1, 1);
+            Piece blackRook = new Rook(Color.BLACK, 6, 1);
+            Piece blackKing = new King(Color.BLACK, 7, 7);
+
+            board.setPosition(whiteKing.row, whiteKing.column, whiteKing.getSymbol());
+            board.setPosition(blackRook.row, blackRook.column, blackRook.getSymbol());
+            board.setPosition(blackKing.row, blackKing.column, blackKing.getSymbol());
+
+            pieces.put(whiteKing.getSymbol(), whiteKing);
+            pieces.put(blackRook.getSymbol(), blackRook);
+            pieces.put(blackKing.getSymbol(), blackKing);
+
+            chess = new Chess(board, 1, pieces);
+
+            assertEquals(false, chess.isTerminal(), "Game should not be terminal with check");
+        }
+
+        //TODO: Add test for checkmate
     }
 
     @Nested
