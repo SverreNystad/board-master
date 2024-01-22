0ebb794e1bd756cb9cc93ec222ce48de6a421eaf
Jon Bergland <jobergl@stud.ntnu.no>
2024-01-22 21:11:42 +0100

    Refactor:  Change tests to comply with changes in Chess.java

--- backend/src/test/java/board/master/model/games/chess/ChessTest.java
+++ backend/src/test/java/board/master/model/games/chess/ChessTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Nested;
 import board.master.model.Action;
 import board.master.model.StateHandler;
 import board.master.model.games.Board;
-import board.master.model.games.Move;
 import board.master.model.games.chess.Pieces.King;
 import board.master.model.games.chess.Pieces.Pawn;
 import board.master.model.games.chess.Pieces.Piece;
@@ -47,11 +46,46 @@ public class ChessTest {
 
     @Test
     void testNoMovesForTerminalState() {
-        StateHandler game = new Chess();
-        while (game.isTerminal() == false) {
-            Action action = game.getActions().get(0);
-            game = game.result(action);
-        }
+        Board board = new Board(8,8);
+        Map<String, Piece> pieces = new HashMap<String, Piece>();
+
+        Piece whiteKing = new King(Color.WHITE, 1, 1);
+        Piece blackRook = new Rook(Color.BLACK, 6, 1);
+        Piece blackKing = new King(Color.BLACK, 7, 7);
+
+        // Place pawns in a ring around the king
+        Piece whitePawn1 = new Pawn(Color.WHITE, 0, 0);
+        Piece whitePawn2 = new Pawn(Color.WHITE, 0, 1);
+        Piece whitePawn3 = new Pawn(Color.WHITE, 0, 2);
+        Piece whitePawn4 = new Pawn(Color.WHITE, 1, 2);
+        Piece whitePawn5 = new Pawn(Color.WHITE, 1, 0);
+        Piece whitePawn6 = new Pawn(Color.WHITE, 2, 0);
+        Piece whitePawn7 = new Pawn(Color.WHITE, 2, 2);
+
+        board.setPosition(whiteKing.row, whiteKing.column, whiteKing.getSymbol());
+        board.setPosition(blackRook.row, blackRook.column, blackRook.getSymbol());
+        board.setPosition(blackKing.row, blackKing.column, blackKing.getSymbol());
+
+        board.setPosition(whitePawn1.row, whitePawn1.column, whitePawn1.getSymbol());
+        board.setPosition(whitePawn2.row, whitePawn2.column, whitePawn2.getSymbol());
+        board.setPosition(whitePawn3.row, whitePawn3.column, whitePawn3.getSymbol());
+        board.setPosition(whitePawn4.row, whitePawn4.column, whitePawn4.getSymbol());
+        board.setPosition(whitePawn5.row, whitePawn5.column, whitePawn5.getSymbol());
+        board.setPosition(whitePawn6.row, whitePawn6.column, whitePawn6.getSymbol());
+        board.setPosition(whitePawn7.row, whitePawn7.column, whitePawn7.getSymbol());
+
+        pieces.put(whiteKing.getSymbol(), whiteKing);
+        pieces.put(blackRook.getSymbol(), blackRook);
+        pieces.put(blackKing.getSymbol(), blackKing);
+        pieces.put(whitePawn1.getSymbol() + 1, whitePawn1);
+        pieces.put(whitePawn2.getSymbol() + 2, whitePawn2);
+        pieces.put(whitePawn3.getSymbol() + 3, whitePawn3);
+        pieces.put(whitePawn4.getSymbol() + 4, whitePawn4);
+        pieces.put(whitePawn5.getSymbol() + 5, whitePawn5);
+        pieces.put(whitePawn6.getSymbol() + 6, whitePawn6);
+        pieces.put(whitePawn7.getSymbol() + 7, whitePawn7);
+
+        StateHandler game = new Chess(board, 1, pieces);
         assertEquals(0, game.getActions().size(), "There should be no possible moves at the end of the game");
     }
 
@@ -146,7 +180,7 @@ public class ChessTest {
         }
 
         @Test
-        void testIsTerminalWithcheckMate() {
+        void testIsTerminalWithCheckMate() {
             Board board = new Board(8,8);
             Map<String, Piece> pieces = new HashMap<String, Piece>();
 
@@ -159,9 +193,9 @@ public class ChessTest {
             Piece whitePawn2 = new Pawn(Color.WHITE, 0, 1);
             Piece whitePawn3 = new Pawn(Color.WHITE, 0, 2);
             Piece whitePawn4 = new Pawn(Color.WHITE, 1, 2);
-            Piece whitePawn5 = new Pawn(Color.WHITE, 2, 0);
-            Piece whitePawn6 = new Pawn(Color.WHITE, 2, 2);
-            Piece whitePawn7 = new Pawn(Color.WHITE, 2, 1);
+            Piece whitePawn5 = new Pawn(Color.WHITE, 1, 0);
+            Piece whitePawn6 = new Pawn(Color.WHITE, 2, 0);
+            Piece whitePawn7 = new Pawn(Color.WHITE, 2, 2);
 
             board.setPosition(whiteKing.row, whiteKing.column, whiteKing.getSymbol());
             board.setPosition(blackRook.row, blackRook.column, blackRook.getSymbol());
@@ -178,13 +212,13 @@ public class ChessTest {
             pieces.put(whiteKing.getSymbol(), whiteKing);
             pieces.put(blackRook.getSymbol(), blackRook);
             pieces.put(blackKing.getSymbol(), blackKing);
-            pieces.put(whitePawn1.getSymbol(), whitePawn1);
-            pieces.put(whitePawn2.getSymbol(), whitePawn2);
-            pieces.put(whitePawn3.getSymbol(), whitePawn3);
-            pieces.put(whitePawn4.getSymbol(), whitePawn4);
-            pieces.put(whitePawn5.getSymbol(), whitePawn5);
-            pieces.put(whitePawn6.getSymbol(), whitePawn6);
-            pieces.put(whitePawn7.getSymbol(), whitePawn7);
+            pieces.put(whitePawn1.getSymbol() + 1, whitePawn1);
+            pieces.put(whitePawn2.getSymbol() + 2, whitePawn2);
+            pieces.put(whitePawn3.getSymbol() + 3, whitePawn3);
+            pieces.put(whitePawn4.getSymbol() + 4, whitePawn4);
+            pieces.put(whitePawn5.getSymbol() + 5, whitePawn5);
+            pieces.put(whitePawn6.getSymbol() + 6, whitePawn6);
+            pieces.put(whitePawn7.getSymbol() + 7, whitePawn7);
 
             chess = new Chess(board, 1, pieces);
             boolean isTerminal = chess.isTerminal();
