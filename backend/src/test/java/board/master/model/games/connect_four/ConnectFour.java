42198decc8905dcbc09874df72409446d05c63e5
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-01-22 19:57:37 +0100

    Feat: Implement ConnectFour class methods for StateHandler interface

--- backend/src/test/java/board/master/model/games/connect_four/ConnectFour.java
+++ backend/src/test/java/board/master/model/games/connect_four/ConnectFour.java
@@ -1,35 +1,44 @@
 package board.master.model.games.connect_four;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import board.master.model.Action;
 import board.master.model.StateHandler;
 import board.master.model.games.Board;
+import board.master.model.games.Move;
 
 public class ConnectFour implements StateHandler {
 
+    private int playerToMove;
+
+    public ConnectFour() {
+        playerToMove = 1;
+    }
+
     @Override
     public int toMove() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toMove'");
+        return playerToMove;
     }
 
     @Override
     public List<Action> getActions() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getActions'");
+        List<Action> actions = new ArrayList<Action>();
+        for (int i = 0; i < 7; i++) {
+            actions.add(new Move(i));
+        }
+        return actions;
     }
 
     @Override
     public StateHandler result(Action action) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'result'");
+        this.playerToMove *= -1;
+        return null;
     }
 
     @Override
     public boolean isTerminal() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isTerminal'");
+        return false;
     }
 
     @Override
