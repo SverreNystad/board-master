fedb2b1dbbbafa2790e1ee7425778975c65517cd
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-13 22:34:36 +0100

    refactor: remove commented out code in MoveRequest

--- backend/src/main/java/board/master/model/communication/MoveRequest.java
+++ backend/src/main/java/board/master/model/communication/MoveRequest.java
@@ -10,11 +10,6 @@ public class MoveRequest {
     private final String gameId;
     private final Move move;
 
-    // public MoveRequest(String gameId, Integer x, Integer y) {
-    //     this.gameId = gameId;
-    //     this.move = new Move(x, y);
-    // }
-
     public MoveRequest(String gameId, String x, String y) {
         this.gameId = gameId;
         this.move = Move.createMove(x, y);
